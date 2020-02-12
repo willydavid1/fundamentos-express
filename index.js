d59250f727deb32c4fd50cd40dd82be459002ee6
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-02-12 16:44:57 -0500

    Settings y Motor de plantillas EJS | usamos EJS que nos permite correr JS dentro de HTML

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const morgan = require("morgan")
 const app = express();
 
 
+// CONFIGURACION
+// Vamos a verlo como una variable que recibe parametros 1-Nombre 2-valor y para obtener la configuracion con app.get("port") /me retorna 3000
+app.set("appName", "WillyExpressAPP")
+app.set("port", 3000)
+app.set("view engine", "ejs") // voy a establecer como motor de plantillas de mi app a ejs
+
+
 // Middlewares - es un manejador de peticion, y se ejecuta antes que lleguen a cualquier ruta | Para usar el Middleware se llaman asi: app.use(logger)
 // const logger = (req, res, next) => {
 //   console.log(`Ruta recibida: ${req.protocol}://${req.get('host')}${req.originalUrl} | en la IP: ${req.ip}`)
@@ -12,11 +19,14 @@ const app = express();
 // }
 
 
+// Middlewares
 // le decimos al servidor que entienda los formatos json, antes de que llegue a cualquier ruta
 app.use(express.json());
 app.use(morgan("dev")) //usa el Middleware morgan es un manejador de peticion y se ejecuta antes que lleguen a cualquier ruta
 // app.use(logger)
 
+
+// RUTAS
 // PARA RECIBIR EN UNA RUTA TODAS LAS PETICIONES HTTP y para seguir con una ruta que se llama igual tengo que decirle next() para que se ejecuten las rutas que tengan ese nombre /user
 // app.all("/user", (req, res, next) => {
 //     console.log("por aqui paso (all)")
@@ -24,6 +34,12 @@ app.use(morgan("dev")) //usa el Middleware morgan es un manejador de peticion y 
 //     next() //continua con la siguiente ruta en este caso /user
 // })
 
+// cuando entren a esta ruta como respuesta se renderiza la vista index.ejs donde le paso datos a la plantilla
+app.get("/", (req, res) => {
+  const data = [{name: "Cameron"}, {name: "joe"}, {name: "ryan"}, {name: "juan"}]
+
+  res.render("index.ejs", {people: data}) // le estoy pasando a la vista un objeto con un atributo people que es un arreglo
+})
 
 // servidor cuando recibas una peticion get en la ruta /user, ejecuta el callback y como respuesta le voy a enviar un json (ese metodo json recibe un objeto)
 app.get("/user", (req, res) => {
@@ -63,6 +79,7 @@ app.use(express.static("public"))
 
 
 // servidor o app quiero que escuches en el puerto 3000 y cuando inicies ejecuta el mensaje por consola
-app.listen(5000, () => {
-  console.log("Servidor express disponible en el puerto 5000");
+app.listen(app.get("port"), () => {
+  console.log(app.get("appName")) //me retorna el valor de la configuracion
+  console.log("Servidor express disponible en el puerto " + app.get("port"));
 });
